6aef6ce43af24cc506cfef057c55994e99dee7c4
Felippe Douglas <87235716+Felippedouglas@users.noreply.github.com>
2022-03-08 17:46:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function ativarDarkTheme() {
-    document.body.style.backgroundColor = "#000"
-    document.getElementById("dark-theme").style.display = "none"
-    document.getElementById("dark-theme-off").style.display = "block"
-}
-
-function desativarDarkTheme() {
-    document.body.style.backgroundColor = "#fff"
-    document.getElementById("dark-theme").style.display = "block"
-    document.getElementById("dark-theme-off").style.display = "none"
-}
-
 function abrirPesquisar() {
     document.getElementById("exibir-search").style.display = "block"
 }
@@ -21,3 +9,39 @@ function fecharPesquisar() {
 function insert(sugestao) {
     document.getElementById("search").value = sugestao
 }
+
+var theme = localStorage.getItem("theme")
+function darkTheme() {
+    theme = '#000'
+    document.getElementById("dark-theme").style.display = "none"
+    document.getElementById("dark-theme-off").style.display = "block"
+    atualizarLocalStorage()
+    verificarTema()
+}
+function lightTheme() {
+    theme = '#fff'
+    document.getElementById("dark-theme").style.display = "block"
+    document.getElementById("dark-theme-off").style.display = "none"
+    atualizarLocalStorage()
+    verificarTema()
+}
+function atualizarLocalStorage() {
+    localStorage.setItem("theme", theme)
+    document.body.style.backgroundColor = theme
+}
+
+setTimeout( function() {
+
+    function verificarTema() {
+        if (theme == '#000') {
+            document.body.style.backgroundColor = "#000"
+            document.getElementById("dark-theme").style.display = "none"
+            document.getElementById("dark-theme-off").style.display = "block"
+        } else {
+            localStorage.setItem("theme", "#fff")
+            document.getElementById("dark-theme").style.display = "block"
+            document.getElementById("dark-theme-off").style.display = "none"
+        }
+    }
+    verificarTema()
+},1)
